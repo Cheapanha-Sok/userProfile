fa0575eff5257a7e329d18f45a57df90d4006f65
Cheapanha-Sok <Cheapanha-Sok>
2024-02-08 11:29:49 +0700

    first commit

---
+++ app/src/main/java/com/example/userprofileui/UserProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.userprofileui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.jetbrains.annotations.NotNull;
+
+public class UserProfileActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@NotNull Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userprofile);
+
+    }
+}
